bd73c11eca53663fe94a6efa1ee7aab875fcd638
Chirag Kachhadiya <chiragkachhadiya000@gmail.com>
2023-03-01 10:47:38 +0530

    Add Admin Panle

--- User/include/contactus.php
+++ User/include/contactus.php
@@ -1,6 +1,6 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <title>Makaan - Real Estate php</title>
@@ -32,93 +32,107 @@
     <link href="../css/style.css" rel="stylesheet">
     <title>Navbar</title>
 </head>
-<body>
-    
-<div class="container-xxl py-5">
-            <div class="container">
-                <div class="text-center text-black mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
-                    <h1 class="mb-3 text-black">Contact Us</h1>
-                    <p>Eirmod sed ipsum dolor sit rebum labore magna erat. Tempor ut dolore lorem kasd vero ipsum sit eirmod sit. Ipsum diam justo sed rebum vero dolor duo.</p>
-                </div>
-                <div class="row g-4">
-                    <div class="col-12">
-                        <div class="row gy-4">
-                            <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.1s">
-                                <div class="bgcolor rounded p-3">
-                                    <div class="d-flex align-items-center bg-white text-black rounded p-3" style="border: 1px dashed var(--tan)">
-                                        <div class="icon icon1 me-3" style="width: 45px; height: 45px;">
-                                            <i class="fa text-tan btn-social fa-map-marker-alt"></i>
-                                        </div>
-                                        <span>MD Park Socity Simada Gam</span>
-                                    </div>
-                                </div>
-                            </div>
-                            
-                            <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.3s">
-                                <div class="bgcolor rounded p-3">
-                                    <div class="d-flex text-black align-items-center bg-white rounded p-3" style="border: 1px dashed var(--tan)">
-                                        <div class="icon  me-3" style="width: 45px; height: 45px;">
-                                            <i class="fa fa-envelope-open text-tan"></i>
-                                        </div>
-                                        <span>locus_support@gmail.com</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.5s">
-                                <div class="bgcolor rounded p-3">
-                                 <div class="d-flex text-black align-items-center bg-white rounded p-3" style="border: 1px dashed var(--tan)">
-                                        <div class="icon me-3" style="width: 45px; height: 45px;">
-                                            <i class="fa fa-phone-alt text-tan"></i>
-                                        </div>
-                                        <span>+91 11223 34455</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7438.6242865312515!2d72.89836772411462!3d21.21946734358138!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be045882435e545%3A0xd6f93d3d2fa5d1b5!2sSimada%20Gam%2C%20Nana%20Varachha%2C%20Surat%2C%20Gujarat%20395006!5e0!3m2!1sen!2sin!4v1677499400853!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="wow fadeInUp" data-wow-delay="0.5s">
-                            <p class="mb-4">Contact Us</a>.</p>
-                            <form action="contact.php" method="post">
 
-                                    <div class="row g-3">
-                                        <div class="col-md-6">
-                                            <div class="form-floating">
-                                                <input type="text" class="form-control" id="name" name="name" placeholder="Your Name">
-                                                <label for="name">Your Name</label>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="form-floating">
-                                                <input type="email" class="form-control" id="email" name="email" placeholder="Your Email">
-                                                <label for="email">Your Email</label>
-                                            </div>
-                                        </div>
-                                        <div class="col-12">
-                                            <div class="form-floating">
-                                                <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
-                                                <label for="subject">Subject</label>
-                                            </div>
-                                        </div>
-                                        <div class="col-12">
-                                            <div class="form-floating">
-                                                <textarea class="form-control" placeholder="Leave a message here" name="message" id="message" style="height: 150px"></textarea>
-                                                <label for="message">Message</label>
-                                            </div>
-                                        </div>
-                                        <div class="col-12">
-                                            <button class="btn bg-tan text-black w-100 py-3" formaction="https://formspree.io/f/myyawllv" name="submit" type="submit">Send Message</button>
-                                        </div>
+<body>
+
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center text-black mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s"
+                style="max-width: 600px;">
+                <h1 class="mb-3 text-black">Contact Us</h1>
+                <p>Eirmod sed ipsum dolor sit rebum labore magna erat. Tempor ut dolore lorem kasd vero ipsum sit eirmod
+                    sit. Ipsum diam justo sed rebum vero dolor duo.</p>
+            </div>
+            <div class="row g-4">
+                <div class="col-12">
+                    <div class="row gy-4">
+                        <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.1s">
+                            <div class="bgcolor rounded p-3">
+                                <div class="d-flex align-items-center bg-white text-black rounded p-3"
+                                    style="border: 1px dashed var(--tan)">
+                                    <div class="icon icon1 me-3" style="width: 45px; height: 45px;">
+                                        <i class="fa text-tan btn-social fa-map-marker-alt"></i>
                                     </div>
-                            </form>
+                                    <span>MD Park Socity Simada Gam</span>
+                                </div>
+                            </div>
                         </div>
+
+                        <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.3s">
+                            <div class="bgcolor rounded p-3">
+                                <div class="d-flex text-black align-items-center bg-white rounded p-3"
+                                    style="border: 1px dashed var(--tan)">
+                                    <div class="icon  me-3" style="width: 45px; height: 45px;">
+                                        <i class="fa fa-envelope-open text-tan"></i>
+                                    </div>
+                                    <span>locus_support@gmail.com</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.5s">
+                            <div class="bgcolor rounded p-3">
+                                <div class="d-flex text-black align-items-center bg-white rounded p-3"
+                                    style="border: 1px dashed var(--tan)">
+                                    <div class="icon me-3" style="width: 45px; height: 45px;">
+                                        <i class="fa fa-phone-alt text-tan"></i>
+                                    </div>
+                                    <span>+91 11223 34455</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7438.6242865312515!2d72.89836772411462!3d21.21946734358138!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be045882435e545%3A0xd6f93d3d2fa5d1b5!2sSimada%20Gam%2C%20Nana%20Varachha%2C%20Surat%2C%20Gujarat%20395006!5e0!3m2!1sen!2sin!4v1677499400853!5m2!1sen!2sin"
+                        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="col-md-6">
+                    <div class="wow fadeInUp" data-wow-delay="0.5s">
+                        <p class="mb-4">Contact Us</a>.</p>
+                        <form action="contact.php" method="post">
+
+                            <div class="row g-3">
+                                <div class="col-md-6">
+                                    <div class="form-floating">
+                                        <input type="text" class="form-control" id="name" name="name"
+                                            placeholder="Your Name">
+                                        <label for="name">Your Name</label>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-floating">
+                                        <input type="email" class="form-control" id="email" name="email"
+                                            placeholder="Your Email">
+                                        <label for="email">Your Email</label>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-floating">
+                                        <input type="text" class="form-control" id="subject" name="subject"
+                                            placeholder="Subject">
+                                        <label for="subject">Subject</label>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-floating">
+                                        <textarea class="form-control" placeholder="Leave a message here" name="message"
+                                            id="message" style="height: 150px"></textarea>
+                                        <label for="message">Message</label>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <button class="btn bg-tan text-black w-100 py-3" name="submit" type="submit">Send
+                                        Message</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
@@ -126,8 +140,9 @@
     <script src="../lib/easing/easing.min.js"></script>
     <script src="../lib/waypoints/waypoints.min.js"></script>
     <script src="../lib/owlcarousel/owl.carousel.min.js"></script>
-    
+
     <!-- Template Javascript -->
     <script src="../js/main.js"></script>
 </body>
+
 </html>
